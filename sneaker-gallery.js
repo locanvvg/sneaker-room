4721132a43ae10979ab73e5bba129a5174cd18f5
locanvvg <locan2635@gmail.com>
2026-09-13 01:00:44 -0400

    Create sneaker-gallery.js

---
+++ sneaker-gallery.js
@@ -0,0 +1,98 @@
+/* =========================================================
+   LỘC AN SNEAKER COLLECTION
+   SNEAKER IMAGE GALLERIES
+
+   - Ảnh đầu tiên luôn là COVER / ảnh bìa.
+   - Các ảnh tiếp theo là DETAIL / ảnh phụ.
+   - Homepage vẫn dùng sneaker.image trong data.js.
+   - File này chỉ phục vụ gallery trên shoe.html.
+========================================================= */
+
+window.SNEAKER_GALLERIES = {
+
+
+  /* =====================================================
+     1. JORDAN 1 BLACK BLOODLINE PE SAMPLE
+  ===================================================== */
+
+  "air-jordan-1-black-bloodline-pe": [
+    "pictures/jordan1_pe_bloodline_friendsandfamily_sample.png"
+  ],
+
+
+  /* =====================================================
+     2. JORDAN 1 QUAI 54 F&F
+
+     Ảnh 1 = cover
+     Ảnh 2 = detail
+  ===================================================== */
+
+  "jordan-1-quai54-ff": [
+    "pictures/jordan1_quai54_ff_v2.png",
+    "pictures/jordan1_quai54_ff_v2_1.png"
+  ],
+
+
+  /* =====================================================
+     3. NIKE SB DUNK LOW YUTO HORIGOME MATCHA
+
+     Ảnh 1 = cover
+     Ảnh 2 = detail
+
+     LƯU Ý:
+     Filename thực tế bạn upload hiện tại là:
+     sbdunk_yutohorigome_matcha1.png
+     không có dấu "_" trước số 1.
+  ===================================================== */
+
+  "nike-sb-dunk-yuto-matcha": [
+    "pictures/sbdunk_yutohorigome_matcha.png",
+    "pictures/sbdunk_yutohorigome_matcha1.png"
+  ],
+
+
+  /* =====================================================
+     4. VIRGIL ABLOH ARCHIVE ALASKA
+  ===================================================== */
+
+  "virgil-abloh-archive-jordan-1-alaska": [
+    "pictures/jordan1_vaa_alaska_v2.png"
+  ],
+
+
+  /* =====================================================
+     5. FRAGMENT x UNION LA SAMPLE
+  ===================================================== */
+
+  "fragment-union-air-jordan-1-varsity-red-sport-royal-sample": [
+    "pictures/jordan1_fragmentunion_sample.png"
+  ],
+
+
+  /* =====================================================
+     6. JORDAN 1 SHADOW 2009
+  ===================================================== */
+
+  "jordan-1-shadow-2009": [
+    "pictures/jordan1_shadow_2009.png"
+  ],
+
+
+  /* =====================================================
+     7. BAPE x STUSSY
+  ===================================================== */
+
+  "bape-stussy-camo-canvas-hi-top-green": [
+    "pictures/bapesta_stussy.png"
+  ],
+
+
+  /* =====================================================
+     8. NIKE x OFF-WHITE WAFFLE RACER
+  ===================================================== */
+
+  "nike-off-white-waffle-racer-white": [
+    "pictures/nike_waffle_racer_ow.png"
+  ]
+
+};
